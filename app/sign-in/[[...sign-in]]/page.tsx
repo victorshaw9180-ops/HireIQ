f0a7d1881d39bb8a3688108bf8a024c3547f6634
Vishal Shah <victor.shaw9180@gmail.com>
2026-05-11 17:15:54 +0530

    Rollback Clerk auth pages

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -4,22 +4,8 @@
 //  return <SignIn afterSignInUrl="/dashboard" />;
 //}
 
-//import { SignIn } from "@clerk/nextjs";
-
-//export default function Page() {
-//  return <SignIn />
-//}
-
 import { SignIn } from "@clerk/nextjs";
 
-export default function SignInPage() {
-  return (
-    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
-      <SignIn
-        routing="path"
-        path="/sign-in"
-        signUpUrl="/sign-up"
-      />
-    </div>
-  );
+export default function Page() {
+  return <SignIn />
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -10,22 +10,8 @@
 //  return <SignUp afterSignUpUrl="/dashboard" />;
 //}
 
-//import { SignUp } from "@clerk/nextjs";
-
-//export default function Page() {
-//  return <SignUp fallbackRedirectUrl="/dashboard" />;
-//}
-
 import { SignUp } from "@clerk/nextjs";
 
-export default function SignUpPage() {
-  return (
-    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
-      <SignUp
-        routing="path"
-        path="/sign-up"
-        signInUrl="/sign-in"
-      />
-    </div>
-  );
+export default function Page() {
+  return <SignUp fallbackRedirectUrl="/dashboard" />;
 }
